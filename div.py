2ffd5b5e460e6f82de23a89d9a00ddc47760c787
ghousiamk <69473074+ghousiamk@users.noreply.github.com>
2020-08-10 22:32:13 -0400

    Add files via upload

---
+++ div.py
@@ -0,0 +1,6 @@
+# div.py
+
+def div(dividend: int, divisor: int) -> int:
+    if divisor == 0:
+        return -1
+    return dividend // divisor
---
+++ increment_decrement.py
@@ -0,0 +1,8 @@
+# increment_decrement.py
+
+def increment(number: int) -> int:
+    return number + 1
+
+
+def decrement(number: int) -> int:
+    return number - 1
